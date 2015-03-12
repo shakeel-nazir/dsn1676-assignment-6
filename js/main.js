f908d34a0f4ad897e4d56f5aedc49e17d6b797f9
shakeel-nazir <shakeelnazir@MacBook-Pro.local>
2015-03-12 13:33:53 -0400

    Add CSS
    - Add CSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-var $submit = $('.submit');
-var $box = $('.box')
-                   
-                   
+var $submit = $('.btn-show-hide');
+var $box = $('.box');
+var $btn = $('.btn-move');
+var $dia = $('.diamond');
+var $btncollapse = $('.btn-collapse-expand');
+var $panel = $('.panel')
+var $circle = $('.circle')       
+var $btnbounce = $('.btn-bounce')
+var $btnAppend = $('.btn-append')
+var $list = $('.list')
+
 $submit.on('click', function () {
-    $box.addClass('js-box-animate');
+    $box.toggleClass('show');
 })
+
+$btn.on('click', function () {
+    $dia.toggleClass('move');
+})
+
+$btncollapse.on('click', function () {
+    $panel.animate({height: 'toggle'});
+})
+
+$btnbounce.on('click', function () {
+    $circle.addClass('js-circle');
+})
+
+
+$circle.on('animationend', function () {
+    $circle.removeClass('js-circle');
+})
+
+
+$btnAppend.on('click', function () {
+    var $li = $('<li>').html('new list item');
+    
+    $list.prepend($li);
+    $li.toggleClass('list-animation');
+});
+
 
